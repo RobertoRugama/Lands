3ef3366070f43162480c47e8e5b09ba03e98c2cc
Roberto Rugama <roberto.rugama01@gmail.com>
2018-11-04 18:56:03 -0600

    Crate LoginPage
    Creating Login page

---
+++ Lands/Lands/Lands/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Lands
+{
+    using Views;
+    using Xamarin.Forms;
+    public partial class App : Application
+    {
+        #region
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LoginPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
